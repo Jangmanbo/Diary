4f8f2d1e64e22c7363a0725c12ca39cfd41f6849
Jangmanbo <wkddpdnjs99@gmail.com>
2021-07-31 20:14:11 +0900

    add PostButton

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     PostAdapter adapter;
+    FloatingActionButton btn;
 
 
     @Override
@@ -16,10 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.floatingActionButton);
         recyclerView = findViewById(R.id.recyclerView);
 
         adapter=new PostAdapter();
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "버튼 클릭", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
