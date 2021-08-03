e4f2228f12b2dfa579bd79ad1bd74e9a9541ad6d
Jangmanbo <wkddpdnjs99@gmail.com>
2021-08-03 23:58:11 +0900

    add the ability to modify post

--- app/src/main/java/com/example/diary/PostActivity.java
+++ app/src/main/java/com/example/diary/PostActivity.java
@@ -1,13 +1,17 @@
 package com.example.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class PostActivity extends AppCompatActivity {
     TextView title, contents;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,23 @@ public class PostActivity extends AppCompatActivity {
 
         title=findViewById(R.id.titleTextView);
         contents=findViewById(R.id.contentsTextView);
-
+        btn=findViewById(R.id.button);
 
         Bundle bundle = getIntent().getExtras();
-        Post post = bundle.getParcelable("post");
+        final Post post = bundle.getParcelable("post");
 
         title.setText(post.getTitle());
         contents.setText(post.getContents());
+
+        //게시글 수정
+        final AppDatabase db= Room.databaseBuilder(this, AppDatabase.class, "Post-db").allowMainThreadQueries().build();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                post.setTitle(title.getText().toString());
+                post.setContents(contents.getText().toString());
+                db.postDao().update(post);
+            }
+        });
     }
 }
