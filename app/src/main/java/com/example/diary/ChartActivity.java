d4eea03d4c3c98785faeb4e154732e99c008729a
Jangmanbo <wkddpdnjs99@gmail.com>
2021-12-10 03:40:33 +0900

    add : sample chart

--- app/src/main/java/com/example/diary/ChartActivity.java
+++ app/src/main/java/com/example/diary/ChartActivity.java
@@ -3,6 +3,7 @@ package com.example.diary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,10 +11,22 @@ import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class ChartActivity extends AppCompatActivity {
+    LineChart lineChart;
     ImageButton calenderBtn;
     TextView calenderTextView;
     int year, month;
@@ -32,7 +45,58 @@ public class ChartActivity extends AppCompatActivity {
         month = cal.get(Calendar.MONTH);
         calenderTextView.setText(year + "." + (month + 1));
 
+        //클릭 리스너 등록
         calenderBtn.setOnClickListener(click);
+
+        //샘플 차트
+        lineChart = (LineChart)findViewById(R.id.chart);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 1));
+        entries.add(new Entry(2, 2));
+        entries.add(new Entry(3, 0));
+        entries.add(new Entry(4, 4));
+        entries.add(new Entry(5, 3));
+        entries.add(new Entry(7, 10));
+
+        LineDataSet lineDataSet = new LineDataSet(entries, "기분");
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setCircleRadius(6);
+        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setCircleColorHole(Color.BLUE);
+        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+        lineDataSet.setDrawHighlightIndicators(false);
+        lineDataSet.setDrawValues(false);
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.enableGridDashedLine(8, 24, 0);
+        xAxis.setLabelCount(7, true);
+        xAxis.setAxisMinimum(1);
+        xAxis.setAxisMaximum(7);
+
+        YAxis yLAxis = lineChart.getAxisLeft();
+        yLAxis.setTextColor(Color.BLACK);
+        YAxis yRAxis = lineChart.getAxisRight();
+        yRAxis.setDrawLabels(false);
+        yRAxis.setDrawAxisLine(false);
+        yRAxis.setDrawGridLines(false);
+        yLAxis.setLabelCount(11, true);
+        yLAxis.setAxisMinimum(0);
+        yLAxis.setAxisMaximum(10);
+
+        Description description = new Description();
+        description.setText("");
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription(description);
+        lineChart.animateY(2000, Easing.EasingOption.EaseInCubic);
+        lineChart.invalidate();
     }
 
     View.OnClickListener click = new View.OnClickListener() {
